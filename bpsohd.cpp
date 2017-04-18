e4a504ea9461c5ba2a85cce03326d0a63b73cdc1
NineDakOldAncestor <zjiezhang@hotmail.com>
2017-04-18 15:30:35 +0800

    Update bpsohd.cpp

--- bpsohd.cpp
+++ bpsohd.cpp
@@ -5,12 +5,12 @@
 #include <algorithm>
 #include <math.h>
 
-/****************************************************************************
+/**********************************************************************************************************************************************************************************
 File: bpsohd.cpp
 Contents: Mining long frequent patterns through an improved binary particle swarm optimization
 Author: Zhongjie Zhang, Jian Huang, Ying Wei
 Citation: Zhang Z, Huang J, Wei Y, et al. Frequent item sets mining from high-dimensional dataset based on a novel binary particle swarm optimization[J]. Journal of Central South University, 2016, 23(7): 1700-1708.
-****************************************************************************/
+**********************************************************************************************************************************************************************************/
 
 using namespace std;
 #pragma warning(disable:4996) 
@@ -29,6 +29,7 @@ int length;//the length of pattern specified by users
 bool validate[BUFFER] = { 0 };//the validate dimension of dataset
 char str[BUFFER];
 int  globali;
+int patternNum = 0;
 
 class Particle//the particle or chromosome
 {
@@ -54,15 +55,55 @@ bool compare(int a, int b)//the operator to sort items in the transaction
 	return Items[a]>Items[b];
 }
 
-void PreScan(FILE *fIn)
+class trans
+{
+public:
+	char *str;
+	trans *next;
+	int len = 0;
+	int tid;
+};
+
+trans *Dataset;//the dataset sotred in the memory
+trans *strans = new trans;
+
+void ReadData(FILE *fIn)
+{
+	transNum = 0;
+	int i, j;
+	char str[BUFFER];
+	Dataset = strans;
+	for (i = 0; fgets(str, BUFFER, fIn); i++)//Counting the support of every candidate itemset
+	{
+		if (transNum > 0)
+		{
+			strans->next = new trans;
+			strans = strans->next;
+		}
+		for (j = 0; j < BUFFER && str[j] != '\0'; j++)
+		{
+
+		}
+		strans->str = new char[j + 1];
+		strans->len = j + 1;
+		memcpy(strans->str, str, (j + 1) * sizeof(char));
+		strans->str[j] = '\0';
+		strans->next = NULL;
+		strans->tid = i;
+		transNum++;
+	}
+}
+
+void PreScan()
 {
 	int value;//the variable to record the item in every transaction
 	int i, j;
 	char * token;
 
-	for (i = 0; fgets(str, BUFFER, fIn); i++)//Counting the support of every item
+	for (strans = Dataset; strans != NULL; strans = strans->next)//Counting the support of every item
 	{
-		transNum++;//the number of transaction plus one
+		//memset(str, 0, BUFFER);
+		memcpy(str, strans->str, strans->len);
 		token = strtok(str, " \t\n");
 		while (token != NULL)
 		{
@@ -131,9 +172,9 @@ void Generate()//initialize the population
 	}
 }
 
-void UpdateResults(FILE *fIn)//Update the fitness, local best solution and the global best solution
+void UpdateResults()//Update the fitness, local best solution and the global best solution
 {
-	rewind(fIn);
+	//rewind(fIn);
 	int value;
 	int i, j, m, transdsize;
 	char * token;
@@ -141,8 +182,10 @@ void UpdateResults(FILE *fIn)//Update the fitness, local best solution and the g
 	{
 		Pop[i].fitness = 0;
 	}
-	for (i = 0; fgets(str, BUFFER, fIn); i++)//scanning the dataset to update the fitness of all the particle           
+	for (strans = Dataset; strans != NULL; strans = strans->next)//scanning the dataset to update the fitness of all the particle           
 	{
+		//memset(str, 0, BUFFER);
+		memcpy(str, strans->str, strans->len);
 		token = strtok(str, " \t\n");
 		int trans[BUFFER] = { 0 };//store the transaction to a vector trans
 		j = 0;
@@ -182,19 +225,21 @@ void UpdateResults(FILE *fIn)//Update the fitness, local best solution and the g
 		if (Pop[i].fitness >= Pop[i].lfitness)//Update local best solution
 		{
 			Pop[i].lfitness = Pop[i].fitness;
-			for (j = 0; j < BUFFER; j++)
-			{
-				Pop[i].lbest[j] = Pop[i].pat[j];
-			}
+			//for (j = 0; j < BUFFER; j++)
+			//{
+			//	Pop[i].lbest[j] = Pop[i].pat[j];
+			//}
+			memcpy(Pop[i].lbest, Pop[i].pat, BUFFER);
 			Pop[i].lLen = Pop[i].patLen;
 		}
 		if (Pop[i].fitness >= gfitness)//Update global best solution
 		{
 			gfitness = Pop[i].fitness;
-			for (j = 0; j < BUFFER; j++)
-			{
-				gbest[j] = Pop[i].pat[j];
-			}
+			//for (j = 0; j < BUFFER; j++)
+			//{
+			//	gbest[j] = Pop[i].pat[j];
+			//}
+			memcpy(gbest, Pop[i].pat, BUFFER);
 		}
 		if (Pop[i].fitness >= minsup*transNum)//Put the pattern into the final solution
 		{
@@ -284,12 +329,12 @@ void UpdatePos()//Update the position of particles
 	}
 }
 
-void Mining(FILE *fIn)
+void Mining()
 {
 	Generate();
 	for (globali = 0; globali < GenNum; globali++)
 	{
-		UpdateResults(fIn);
+		UpdateResults();
 		UpdatePos();
 		Reduce();
 		printf("generation %d is completed\n", globali + 1);
@@ -305,23 +350,60 @@ void Mining(FILE *fIn)
 			}
 		}
 		printf("support %f\n", Outsupp[i]);
+		patternNum++;
 	}
 }
 
-void main()
+int main(int argc, char** argv)
 {
-	minsup = 0.00001;//setting the minimum support
-	length = 10;//setting the minimum length for the final patterns
-	Popsize = 20;//setting the size of population
-	omiga = 0.5;//setting the inertia weight
-	c1 = 1;//setting the first acceleration
-	c2 = 1;//setting the second acceleration
-	GenNum = 30;//setting the number of generation
 	FILE *fIn;
-	if (!(fIn = fopen("kosarak.dat", "r")))//input the data for mining
+	if (argc != 9)
+	{
+		printf("Usage: bpsohd <input-filename> <minimum-support> <population-size> <generation-number> <inertia-weight> <acceleration-1> <acceleration-2> <length>\n");
+	}
+	else if (!(fIn = fopen(argv[1], "r")))
 	{
 		printf("Error in input file\n");
 	}
-	PreScan(fIn);
-	Mining(fIn);
+	else if (atof(argv[2]) < 0)
+	{
+		printf("invalid minimum support");
+	}
+	else if (atoi(argv[3]) < 0)
+	{
+		printf("invalid population size");
+	}
+	else if (atoi(argv[4]) < 0)
+	{
+		printf("invalid generation number");
+	}
+	else if (atof(argv[5]) < 0)
+	{
+		printf("invalid inertia weight");
+	}
+	else if (atof(argv[6]) < 0 || atof(argv[7]) < 0)
+	{
+		printf("invalid acceleration");
+	}
+	else if (atoi(argv[8]) < 0)
+	{
+		printf("invalid length");
+	}
+	else
+	{
+		minsup = atof(argv[2]);//setting the minimum support
+		length = atoi(argv[8]);//setting the minimum length for the final patterns
+		Popsize = atoi(argv[3]);//setting the size of population
+		omiga = atof(argv[5]);//setting the inertia weight
+		c1 = atof(argv[6]);//setting the first acceleration
+		c2 = atof(argv[7]);//setting the second acceleration
+		GenNum = atoi(argv[4]);//setting the number of generation
+		ReadData(fIn);
+		PreScan();
+		Mining();
+		printf("\n%d patterns are output\n", patternNum);
+	}
+	printf("push retrun to exit");
+	getchar();
+	return 0;
 }
