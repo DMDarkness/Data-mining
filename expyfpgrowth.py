8f0608fe364004ca133031588528d4d49a299b35
NineDakOldAncestor <zjiezhang@hotmail.com>
2017-04-24 13:50:40 +0800

    Create expyfpgrowth.py

---
+++ expyfpgrowth.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 24 00:10:17 2017
+
+@author: ZhongjieZhang
+"""
+import pyfpgrowth
+
+fi = pyfpgrowth.fpgrowth([[1],[1,3],[2,6,7],[1,5],[2,5,6],[1,2,7]],0.2)
