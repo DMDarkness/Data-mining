1c0606ebc056aa77da691165f5f89a67665355c1
NineDakOldAncestor <zjiezhang@hotmail.com>
2017-05-02 16:41:55 +0800

    optimize the fp2ar

--- fp2ar.py
+++ fp2ar.py
@@ -8,6 +8,18 @@ Transforming the frequent itemsets to the association rules based
 on the specificed minimum confidence and minimum lift.
 """
 
+"""
+Getting all the subsets with length L and len(pat)-L of the object pattern pat
+"""
+def getSubSet(pat,sub1,sub2,L,temp,base):
+    if(L<1):
+        if(sub1.count(temp)==0):
+            sub1.append(temp)
+            sub2.append(pat)
+    else:
+        for i in range(base,len(pat)):
+            getSubSet(pat[0:i]+pat[i+1:],sub1,sub2,L-1,temp+[pat[i]], i)
+
 def getAR(fi,N,minconf,minlift):
     Rule=[]
     fiNum=len(fi)
@@ -17,30 +29,28 @@ def getAR(fi,N,minconf,minlift):
         fii=fi[i][0:-1]
         fii.sort()
         fid[tuple(fii)]=fi[i][-1]
-    #scan the frequent patterns to form the association rules
     for i in range(0,fiNum):
-        pat1=fi[i][0:-1]
-        pat1.sort()
-        sp1=fi[i][-1]
-        for j in range(i+1,fiNum):
-            if(len(fi[j])!=len(fi[i])):
-                pat2=fi[j][0:-1]
-                pat2.sort()
-                sp2=fi[j][-1]
-                #to check whether pat1 is contained by pat2
-                if(len(set(pat1).difference(pat2))==0):
-                    pat3=list(set(pat2).difference(pat1))
-                    pat3.sort()
-                    sp3=fid[tuple(pat3)]
-                    #to check whether this rule satisfied the criteria
-                    if(1.0*sp2/sp1>minconf and 1.0*N*sp2/(sp1*sp3)>minlift):
-                        Rule.append([pat1,pat3, sp2, 1.0*sp2/sp1, 1.0*N*sp2/(sp1*sp3)])
-                #to check whether pat2 is contained by pat1
-                if(len(set(pat2).difference(pat1))==0):
-                    pat3=list(set(pat1).difference(pat2))
-                    pat3.sort()
-                    sp3=fid[tuple(pat3)]
-                    if(1.0*sp1/sp2>minconf and 1.0*N*sp1/(sp2*sp3)>minlift):
-                        Rule.append([pat2,pat3, sp1, 1.0*sp1/sp2, 1.0*N*sp1/(sp2*sp3)])
+        psLen=int((len(fi[i])-1)/2)
+        pat=fi[i][0:-1]
+        sup1=fi[i][-1]
+        sub1=[]
+        sub2=[]
+        #find all the subset of pat
+        for j in range(1,psLen+1):
+            getSubSet(pat,sub1,sub2,j,[],0)
+        for j in range(0,len(sub1)):
+            pat2=sub1[j]
+            pat3=sub2[j]
+            pat2.sort()
+            pat3.sort()
+            sup2=fid[tuple(pat2)]
+            sup3=fid[tuple(pat3)]
+            #check the lift
+            if(sup1*N*1.0/(sup2*sup3*1.0) > minlift):
+                #check the confidence
+                if(sup1*1.0/sup2 > minconf):
+                    Rule.append([pat2,pat3,sup1,sup1*1.0/sup2,sup1*N*1.0/(sup2*sup3*1.0)])
+                if(sup1*1.0/sup3 > minconf):
+                    Rule.append([pat3,pat2,sup1,sup1*1.0/sup3,sup1*N*1.0/(sup2*sup3*1.0)])      
     return Rule
                 
